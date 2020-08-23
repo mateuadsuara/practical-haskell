ba42644b18bd7349cf6f3c16d57c990a84d75a72
Mateu Adsuara <mateuadsuara@gmail.com>
2020-08-23 18:29:18 +0200

    Implement Show typeclass for Client

--- src/Store.hs
+++ src/Store.hs
@@ -3,3 +3,4 @@ module Store (Client) where
 data Client = GovOrg     String
             | Company    String Integer String String
             | Individual String String Bool
+            deriving Show
