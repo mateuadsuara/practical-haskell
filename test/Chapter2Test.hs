6ad49fb7d9607cad2d83421fab02626102d04bc4
Mateu Adsuara <mateuadsuara@gmail.com>
2020-08-23 17:39:50 +0200

    Generalise test function

--- test/Chapter2Test.hs
+++ test/Chapter2Test.hs
@@ -8,72 +8,94 @@ import Chapter2.SimpleFunctions
 main :: IO ()
 main = do
   test ( "2.1.a"
-       , Chapter2.exercise_2_1_a == ["abc", "de"]
+       , Chapter2.exercise_2_1_a
+       , (==)
+       , ["abc", "de"]
        )
   test ( "2.1.b 1"
-       , (Chapter2.exercise_2_1_b []) == True
+       , Chapter2.exercise_2_1_b []
+       , (==)
+       , True
        )
   test ( "2.1.b 2"
-       , (Chapter2.exercise_2_1_b [[], ['a', 'b']]) == True
+       , Chapter2.exercise_2_1_b [[], ['a', 'b']]
+       , (==)
+       , True
        )
   test ( "2.1.b 3"
-       , (Chapter2.exercise_2_1_b [['a', 'b']]) == False
+       , Chapter2.exercise_2_1_b [['a', 'b']]
+       , (==)
+       , False
        )
   test ( "2.1.c 1"
-       , (Chapter2.exercise_2_1_c []) == False
+       , Chapter2.exercise_2_1_c []
+       , (==)
+       , False
        )
   test ( "2.1.c 2"
-       , (Chapter2.exercise_2_1_c ['a']) == True
+       , Chapter2.exercise_2_1_c ['a']
+       , (==)
+       , True
        )
   test ( "2.1.c 3"
-       , (Chapter2.exercise_2_1_c ['a', 'b']) == False
+       , Chapter2.exercise_2_1_c ['a', 'b']
+       , (==)
+       , False
        )
   test ( "2.1.d"
-       , (Chapter2.exercise_2_1_d ["abc", "de"]) == "abcde"
+       , Chapter2.exercise_2_1_d ["abc", "de"]
+       , (==)
+       , "abcde"
        )
   test ( "firstOrEmpty first"
-       , (firstOrEmpty ["abc"]) == "abc"
+       , firstOrEmpty ["abc"]
+       , (==)
+       , "abc"
        )
   test ( "firstOrEmpty empty"
-       , (firstOrEmpty []) == "empty"
+       , firstOrEmpty []
+       , (==)
+       , "empty"
        )
   test ( "+++ empty"
-       , ([] +++ ["abc"]) == ["abc"]
-       )
-  test ( "+++ two"
-       , (["abc"] +++ ["de"]) == ["abc", "de"]
-       )
-  tsEq ( "reverse2 empty"
-       , (reverse2 [])
-       , []
-       )
-  tsEq ( "reverse2 one"
-       , (reverse2 ["abc"])
+       , [] +++ ["abc"]
+       , (==)
        , ["abc"]
        )
-  tsEq ( "reverse2 two"
-       , (reverse2 ["abc", "de"])
+  test ( "+++ two"
+       , ["abc"] +++ ["de"]
+       , (==)
+       , ["abc", "de"]
+       )
+  test ( "reverse2 empty"
+       , reverse2 []
+       , (==)
+       , []
+       )
+  test ( "reverse2 one"
+       , reverse2 ["abc"]
+       , (==)
+       , ["abc"]
+       )
+  test ( "reverse2 two"
+       , reverse2 ["abc", "de"]
+       , (==)
        , ["de", "abc"]
        )
-  tsEq ( "reverse2 three"
-       , (reverse2 ["abc", "de", "fg"])
+  test ( "reverse2 three"
+       , reverse2 ["abc", "de", "fg"]
+       , (==)
        , ["fg", "de", "abc"]
        )
-  tsEq ( "reverse2 many"
-       , (reverse2 ["1", "2", "3", "4", "5", "6", "7", "8", "9"])
+  test ( "reverse2 many"
+       , reverse2 ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+       , (==)
        , ["9", "8", "7", "6", "5", "4", "3", "2", "1"]
        )
 
-test (d, b) =
-  if b
+test (d, r, f, e) =
+  if f r e
   then putStrLn $ "PASS " ++ d
   else do
-    putStrLn $ "FAIL " ++ d
-    exitFailure
-
-tsEq (d, r, e) =
-  if r == e
-  then putStrLn $ "PASS " ++ d
-  else do
-    putStrLn $ "FAIL " ++ d ++ ", got: " ++ (show r)
+    putStrLn $ "FAIL " ++ d ++ ", result is: " ++ (show r)
     exitFailure
