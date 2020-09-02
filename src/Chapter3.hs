8bfd40bd356124854ef532ed713cfb69b9dea7f0
Mateu Adsuara <mateuadsuara@gmail.com>
2020-09-03 00:04:44 +0200

    Add types to filter', foldr' and foldl'

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -4,13 +4,16 @@ module Chapter3
   , foldl'
   ) where
 
+filter' :: (a -> Bool) -> [a] -> [a]
 filter' f [] = []
 filter' f (x:xs) = if f x
                    then x : (filter f xs )
                    else (filter f xs)
 
+foldr' :: (a -> b -> b) -> b -> [a] -> b
 foldr' f init [] = init
 foldr' f init (x:xs) = f x (foldr' f init xs)
 
+foldl' :: (b -> a -> b) -> b -> [a] -> b
 foldl' f init [] = init
 foldl' f init (x:xs) = f (foldl' f init xs) x
