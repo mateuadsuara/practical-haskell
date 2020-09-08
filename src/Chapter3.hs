8acb8a9bf012777837034ac999d2270623c945b5
Mateu Adsuara <mateuadsuara@gmail.com>
2020-09-08 21:29:59 +0200

    Add map', mapAsFold and induction proof that map' == mapAsFold

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -41,3 +41,40 @@ minimumBy fn lst = foldr' aggFn Nothing lst
 
 elem' :: (Eq a) => a -> [a] -> Bool
 elem' e l = find (== e) l /= Nothing
+
+map' :: (a -> b) -> [a] -> [b]
+map' f [] = []
+map' f (x:xs) = f x : map' f xs
+
+mapAsFold :: (a -> b) -> [a] -> [b]
+mapAsFold f = foldr (\x l -> f x : l) []
+
+-- PROOF
+-- map (is aliased map') == mapAsFold (given foldr is aliased foldr')
+-------------------------
+--
+--    mapAsFold f             = foldr (\x l -> f x : l) []
+-- == mapAsFold f (x:xs)      = foldr (\x l -> f x : l) [] (x:xs)
+--
+-- BASE CASE
+-------------------------
+--
+--    map f []                = []
+-- => []
+--
+--    mapAsFold f []          = foldr (\x l -> f x : l) [] []
+-- => foldr f=>_ init=>[] []  = init=>[]
+-- => []
+--
+-- LONGER LIST (x:xs)
+-------------------------
+--
+--    map f (x:xs)            = f x : map f xs
+-- => f x : RECURSE_WITH xs
+--
+--    mapAsFold f (x:xs)      = foldr (\x l -> f x : l) [] (x:xs)
+-- => foldr f'=>(\x l -> f x : l) init=>[] (x:xs)
+--      = f'=>(\x l -> f x : l) x (foldr f init=>[] xs)
+-- => (\x l -> f x : l) x (foldr f [] xs)
+--      = f x : (foldr f [] xs)
+-- => f x : RECURSE_WITH xs
