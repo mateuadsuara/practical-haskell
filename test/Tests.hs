5bf704769cd79aef4eb05d8f1be5d46f1e462872
Mateu Adsuara <mateuadsuara@gmail.com>
2020-09-12 13:21:28 +0200

    Migrate to Hspec

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import System.Exit
+import Test.Hspec
 
 import Chapter2
 import Chapter2.SimpleFunctions
@@ -9,200 +9,200 @@ import Store.TimeMachine
 import Chapter3
 
 main :: IO ()
-main = do
-  test "2.1.a"
+main = hspec $ do
+  it "2.1.a" $ do
        Chapter2.exercise_2_1_a
-       (==)
+       `shouldBe`
        ["abc", "de"]
 
-  test "2.1.b 1"
-       (Chapter2.exercise_2_1_b [])
-       (==)
+  it "2.1.b" $ do
+       Chapter2.exercise_2_1_b []
+       `shouldBe`
        True
 
-  test "2.1.b 2"
-       (Chapter2.exercise_2_1_b [[], ['a', 'b']])
-       (==)
+  it "2.1.b 2" $ do
+       Chapter2.exercise_2_1_b [[], ['a', 'b']]
+       `shouldBe`
        True
 
-  test "2.1.b 3"
-       (Chapter2.exercise_2_1_b [['a', 'b']])
-       (==)
+  it "2.1.b 3" $ do
+       Chapter2.exercise_2_1_b [['a', 'b']]
+       `shouldBe`
        False
 
-  test "2.1.c 1"
-       (Chapter2.exercise_2_1_c [])
-       (==)
+  it "2.1.c 1" $ do
+       Chapter2.exercise_2_1_c []
+       `shouldBe`
        False
 
-  test "2.1.c 2"
-       (Chapter2.exercise_2_1_c ['a'])
-       (==)
+  it "2.1.c 2" $ do
+       Chapter2.exercise_2_1_c ['a']
+       `shouldBe`
        True
 
-  test "2.1.c 3"
-       (Chapter2.exercise_2_1_c ['a', 'b'])
-       (==)
+  it "2.1.c 3" $ do
+       Chapter2.exercise_2_1_c ['a', 'b']
+       `shouldBe`
        False
 
-  test "2.1.d"
-       (Chapter2.exercise_2_1_d ["abc", "de"])
-       (==)
+  it "2.1.d" $ do
+       Chapter2.exercise_2_1_d ["abc", "de"]
+       `shouldBe`
        "abcde"
 
-  test "firstOrEmpty first"
-       (firstOrEmpty ["abc"])
-       (==)
+  it "firstOrEmpty first" $ do
+       firstOrEmpty ["abc"]
+       `shouldBe`
        "abc"
 
-  test "firstOrEmpty empty"
-       (firstOrEmpty [])
-       (==)
+  it "firstOrEmpty empty" $ do
+       firstOrEmpty []
+       `shouldBe`
        "empty"
 
-  test "+++ empty"
-       ([] +++ ["abc"])
-       (==)
+  it "+++ empty" $ do
+       [] +++ ["abc"]
+       `shouldBe`
        ["abc"]
 
-  test "+++ two"
-       (["abc"] +++ ["de"])
-       (==)
+  it "+++ two" $ do
+       ["abc"] +++ ["de"]
+       `shouldBe`
        ["abc", "de"]
 
-  test "reverse2 empty"
-       (reverse2 [] :: [String])
-       (==)
+  it "reverse2 empty" $ do
+       reverse2 [] :: [String]
+       `shouldBe`
        []
 
-  test "reverse2 one"
-       (reverse2 ["abc"])
-       (==)
+  it "reverse2 one" $ do
+       reverse2 ["abc"]
+       `shouldBe`
        ["abc"]
 
-  test "reverse2 two"
-       (reverse2 ["abc", "de"])
-       (==)
+  it "reverse2 two" $ do
+       reverse2 ["abc", "de"]
+       `shouldBe`
        ["de", "abc"]
 
-  test "reverse2 three"
-       (reverse2 ["abc", "de", "fg"])
-       (==)
+  it "reverse2 three" $ do
+       reverse2 ["abc", "de", "fg"]
+       `shouldBe`
        ["fg", "de", "abc"]
 
-  test "reverse2 many"
-       (reverse2 ["1", "2", "3", "4", "5", "6", "7", "8", "9"])
-       (==)
+  it "reverse2 many" $ do
+       reverse2 ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+       `shouldBe`
        ["9", "8", "7", "6", "5", "4", "3", "2", "1"]
 
-  test "maxmin empty"
-       (maxmin (0, 0) [])
-       (==)
+  it "maxmin empty" $ do
+       maxmin (0, 0) []
+       `shouldBe`
        (0, 0)
 
-  test "maxmin one a"
-       (maxmin (0, 2) [1])
-       (==)
+  it "maxmin one a" $ do
+       maxmin (0, 2) [1]
+       `shouldBe`
        (1, 1)
 
-  test "maxmin one b"
-       (maxmin (2, 0) [1])
-       (==)
+  it "maxmin one b" $ do
+       maxmin (2, 0) [1]
+       `shouldBe`
        (2, 0)
 
-  test "maxmin two"
-       (maxmin (-999, 999) [2, 1])
-       (==)
+  it "maxmin two" $ do
+       maxmin (-999, 999) [2, 1]
+       `shouldBe`
        (2, 1)
 
-  test "maxmin many"
-       (maxmin (-999, 999) [2, 1, 103, 8, 75, -300])
-       (==)
+  it "maxmin many" $ do
+       maxmin (-999, 999) [2, 1, 103, 8, 75, -300]
+       `shouldBe`
        (103, -300)
 
-  test "show client"
-       (show $ Individual (Person "Jack" "Smith" Male))
-       (==)
+  it "show client" $ do
+       show $ Individual (Person "Jack" "Smith" Male)
+       `shouldBe`
        "Individual (Person \"Jack\" \"Smith\" Male)"
 
-  test "clientName GovOrg"
-       (clientName $ GovOrg "organisation")
-       (==)
+  it "clientName GovOrg" $ do
+       clientName $ GovOrg "organisation"
+       `shouldBe`
        "organisation"
 
-  test "clientName Company"
-       (clientName $ Company "Comp Limited" 1 (Person "Jane" "Doe" Female) "Resp")
-       (==)
+  it "clientName Company" $ do
+       clientName $ Company "Comp Limited" 1 (Person "Jane" "Doe" Female) "Resp"
+       `shouldBe`
        "Comp Limited"
 
-  test "clientName Individual"
-       (clientName $ Individual (Person "Jack" "Smith" Male))
-       (==)
+  it "clientName Individual" $ do
+       clientName $ Individual (Person "Jack" "Smith" Male)
+       `shouldBe`
        "Jack Smith"
 
-  test "show time machine"
-       (show $ TimeMachine { manufacturer = "Tesla"
-                           , model = 3
-                           , name = "The E in SEXY"
-                           , travelDirection = Future
-                           , price = 35000.00
-                           })
-       (==)
+  it "show time machine" $ do
+       show $ TimeMachine { manufacturer = "Tesla"
+                          , model = 3
+                          , name = "The E in SEXY"
+                          , travelDirection = Future
+                          , price = 35000.00
+                          }
+       `shouldBe`
        "TimeMachine {manufacturer = \"Tesla\", model = 3, name = \"The E in SEXY\", travelDirection = Future, price = 35000.0}"
 
-  test "countGenders empty"
-       (countGenders [])
-       (==)
+  it "countGenders empty" $ do
+       countGenders []
+       `shouldBe`
        (0, 0, 0)
 
-  test "countGenders one male"
-       (countGenders [(Individual (Person "A" "B" Male))])
-       (==)
+  it "countGenders one male" $ do
+       countGenders [(Individual (Person "A" "B" Male))]
+       `shouldBe`
        (1, 0, 0)
 
-  test "countGenders one female"
-       (countGenders [(Individual (Person "A" "B" Female))])
-       (==)
+  it "countGenders one female" $ do
+       countGenders [(Individual (Person "A" "B" Female))]
+       `shouldBe`
        (0, 1, 0)
 
-  test "countGenders one unknown"
-       (countGenders [(Individual (Person "A" "B" Unknown))])
-       (==)
+  it "countGenders one unknown" $ do
+       countGenders [(Individual (Person "A" "B" Unknown))]
+       `shouldBe`
        (0, 0, 1)
 
-  test "countGenders GovOrg"
-       (countGenders [(GovOrg "A")])
-       (==)
+  it "countGenders GovOrg" $ do
+       countGenders [(GovOrg "A")]
+       `shouldBe`
        (0, 0, 0)
 
-  test "countGenders Company"
-       (countGenders [(Company "A" 1 (Person "A" "B" Male) "B")])
-       (==)
+  it "countGenders Company" $ do
+       countGenders [(Company "A" 1 (Person "A" "B" Male) "B")]
+       `shouldBe`
        (0, 0, 0)
 
-  test "countGenders one of each"
-       (countGenders [ (Individual (Person "A" "B" Male))
-                     , (Individual (Person "A" "B" Female))
-                     , (Individual (Person "A" "B" Unknown))
-                     ])
-       (==)
+  it "countGenders one of each" $ do
+       countGenders [ (Individual (Person "A" "B" Male))
+                    , (Individual (Person "A" "B" Female))
+                    , (Individual (Person "A" "B" Unknown))
+                    ]
+       `shouldBe`
        (1, 1, 1)
 
-  test "applyPriceMultiplier 50% to two TimeMachines"
-       (applyPriceMultiplier 0.5 [ TimeMachine { manufacturer = "Tesla"
-                                               , model = 3
-                                               , name = "The E in SEXY"
-                                               , travelDirection = Future
-                                               , price = 35000.00
-                                               }
-                                 , TimeMachine { manufacturer = "Tesla"
-                                               , model = 1
-                                               , name = "The mass market"
-                                               , travelDirection = Future
-                                               , price = 15000.00
-                                               }
-                                 ])
-       (==)
+  it "applyPriceMultiplier 50% to two TimeMachines" $ do
+       applyPriceMultiplier 0.5 [ TimeMachine { manufacturer = "Tesla"
+                                              , model = 3
+                                              , name = "The E in SEXY"
+                                              , travelDirection = Future
+                                              , price = 35000.00
+                                              }
+                                , TimeMachine { manufacturer = "Tesla"
+                                              , model = 1
+                                              , name = "The mass market"
+                                              , travelDirection = Future
+                                              , price = 15000.00
+                                              }
+                                ]
+       `shouldBe`
        [ TimeMachine { manufacturer = "Tesla"
                      , model = 3
                      , name = "The E in SEXY"
@@ -217,222 +217,214 @@ main = do
                      }
        ]
 
-  test "null' empty"
-       (null' [])
-       (==)
+  it "null' empty" $ do
+       null' []
+       `shouldBe`
        True
 
-  test "null' one"
-       (null' [1])
-       (==)
+  it "null' one" $ do
+       null' [1]
+       `shouldBe`
        False
 
-  test "head' one"
-       (head' [1])
-       (==)
+  it "head' one" $ do
+       head' [1]
+       `shouldBe`
        1
 
-  test "tail' three"
-       (tail' [1, 2, 3])
-       (==)
+  it "tail' three" $ do
+       tail' [1, 2, 3]
+       `shouldBe`
        [2, 3]
 
-  test "ackermann 0 0"
-       (ackermann 0 0)
-       (==)
+  it "ackermann 0 0" $ do
+       ackermann 0 0
+       `shouldBe`
        1
 
-  test "ackermann 0 1"
-       (ackermann 0 1)
-       (==)
+  it "ackermann 0 1" $ do
+       ackermann 0 1
+       `shouldBe`
        2
 
-  test "ackermann 1 0"
-       (ackermann 1 0)
-       (==)
+  it "ackermann 1 0" $ do
+       ackermann 1 0
+       `shouldBe`
        2
 
-  test "ackermann 1 1"
-       (ackermann 1 1)
-       (==)
+  it "ackermann 1 1" $ do
+       ackermann 1 1
+       `shouldBe`
        3
 
-  test "ackermann 0 2"
-       (ackermann 0 2)
-       (==)
+  it "ackermann 0 2" $ do
+       ackermann 0 2
+       `shouldBe`
        3
 
-  test "ackermann 2 0"
-       (ackermann 2 0)
-       (==)
+  it "ackermann 2 0" $ do
+       ackermann 2 0
+       `shouldBe`
        3
 
-  test "ackermann 1 2"
-       (ackermann 1 2)
-       (==)
+  it "ackermann 1 2" $ do
+       ackermann 1 2
+       `shouldBe`
        4
 
-  test "ackermann 2 1"
-       (ackermann 2 1)
-       (==)
+  it "ackermann 2 1" $ do
+       ackermann 2 1
+       `shouldBe`
        5
 
-  test "ackermann 2 2"
-       (ackermann 2 2)
-       (==)
+  it "ackermann 2 2" $ do
+       ackermann 2 2
+       `shouldBe`
        7
 
-  test "unzip' empty"
-       (unzip' [] :: ([Int], [String]))
-       (==)
+  it "unzip' empty" $ do
+       unzip' [] :: ([Int], [String])
+       `shouldBe`
        ([], [])
 
-  test "unzip' one"
-       (unzip' [(1, "a")])
-       (==)
+  it "unzip' one" $ do
+       unzip' [(1, "a")]
+       `shouldBe`
        ([1], ["a"])
 
-  test "unzip' two"
-       (unzip' [(1, "a"), (2, "b")])
-       (==)
+  it "unzip' two" $ do
+       unzip' [(1, "a"), (2, "b")]
+       `shouldBe`
        ([1, 2], ["a", "b"])
 
-  test "filterOnes"
-       (filterOnes [1, 4, 2, 1, 2, 3, 6])
-       (==)
+  it "filterOnes" $ do
+       filterOnes [1, 4, 2, 1, 2, 3, 6]
+       `shouldBe`
        [1, 1]
 
-  test "filterNumber"
-       (filterNumber 5 [1, 2, 3, 4, 5, 6, 7, 8, 1, 3, 5, 7])
-       (==)
+  it "filterNumber" $ do
+       filterNumber 5 [1, 2, 3, 4, 5, 6, 7, 8, 1, 3, 5, 7]
+       `shouldBe`
        [5, 5]
 
-  test "filterNot"
-       (filterNot (\x -> x == 1) [1, 2, 3])
-       (==)
+  it "filterNot" $ do
+       filterNot (\x -> x == 1) [1, 2, 3]
+       `shouldBe`
        [2, 3]
 
-  test "filterGovOrgs"
-       (filterGovOrgs [ (Company "A" 1 (Person "A" "B" Male) "B")
-                      , (GovOrg "A")
-                      , (Individual (Person "A" "B" Unknown))
-                      ])
-       (==)
+  it "filterGovOrgs" $ do
+       filterGovOrgs [ (Company "A" 1 (Person "A" "B" Male) "B")
+                     , (GovOrg "A")
+                     , (Individual (Person "A" "B" Unknown))
+                     ]
+       `shouldBe`
        [ (GovOrg "A") ]
 
-  test "filter'"
-       (filter' (\x -> 0 == (mod x 2)) [1, 2, 3, 4, 5, 6])
-       (==)
+  it "filter'" $ do
+       filter' (\x -> 0 == (mod x 2)) [1, 2, 3, 4, 5, 6]
+       `shouldBe`
        [2, 4, 6]
 
-  test "foldr'"
-       (foldr' (+) 0 [1, 2, 3, 4])
-       (==)
+  it "foldr'" $ do
+       foldr' (+) 0 [1, 2, 3, 4]
+       `shouldBe`
        10
 
-  test "foldl'"
-       (foldr' (+) 0 [1, 2, 3, 4])
-       (==)
+  it "foldl'" $ do
+       foldr' (+) 0 [1, 2, 3, 4]
+       `shouldBe`
        10
 
-  test "product' empty"
-       (product' [])
-       (==)
+  it "product' empty" $ do
+       product' []
+       `shouldBe`
        1
 
-  test "product' one"
-       (product' [7])
-       (==)
+  it "product' one" $ do
+       product' [7]
+       `shouldBe`
        7
 
-  test "product' many"
-       (product' [1, 2, 3, 4, 5])
-       (==)
+  it "product' many" $ do
+       product' [1, 2, 3, 4, 5]
+       `shouldBe`
        120
 
-  test "minimumClient empty"
-       (minimumClient [])
-       (==)
+  it "minimumClient empty" $ do
+       minimumClient []
+       `shouldBe`
        Nothing
 
-  test "minimumClient one"
-       (minimumClient [ (Individual (Person "A" "B" Unknown))
-                      ])
-       (==)
-       (Just (Individual (Person "A" "B" Unknown)))
+  it "minimumClient one" $ do
+       minimumClient [ (Individual (Person "A" "B" Unknown))
+                     ]
+       `shouldBe`
+       Just (Individual (Person "A" "B" Unknown))
 
-  test "minimumClient three"
-       (minimumClient [ (Company "ABCDE" 1 (Person "A" "B" Male) "B")
-                      , (GovOrg "ABC")
-                      , (Individual (Person "A" "BC" Unknown))
-                      ])
-       (==)
-       (Just (GovOrg "ABC"))
+  it "minimumClient three" $ do
+       minimumClient [ (Company "ABCDE" 1 (Person "A" "B" Male) "B")
+                     , (GovOrg "ABC")
+                     , (Individual (Person "A" "BC" Unknown))
+                     ]
+       `shouldBe`
+       Just (GovOrg "ABC")
 
-  test "all' empty"
-       (all' [])
-       (==)
+  it "all' empty" $ do
+       all' []
+       `shouldBe`
        True
 
-  test "all' one false"
-       (all' [False])
-       (==)
+  it "all' one false" $ do
+       all' [False]
+       `shouldBe`
        False
 
-  test "all' one true"
-       (all' [True])
-       (==)
+  it "all' one true" $ do
+       all' [True]
+       `shouldBe`
        True
 
-  test "all' many true"
-       (all' [True, True, True, True, True])
-       (==)
+  it "all' many true" $ do
+       all' [True, True, True, True, True]
+       `shouldBe`
        True
 
-  test "all' almost all true"
-       (all' [True, True, True, False, True])
-       (==)
+  it "all' almost all true" $ do
+       all' [True, True, True, False, True]
+       `shouldBe`
        False
 
-  test "minimumBy empty"
-       (minimumBy (\x -> -x) [])
-       (==)
+  it "minimumBy empty" $ do
+       minimumBy (\x -> -x) []
+       `shouldBe`
        Nothing
 
-  test "minimumBy one"
-       (minimumBy (\x -> -x) [1])
-       (==)
-       (Just 1)
+  it "minimumBy one" $ do
+       minimumBy (\x -> -x) [1]
+       `shouldBe`
+       Just 1
 
-  test "minimumBy three"
-       (minimumBy (\x -> -x) [1, 2, 3])
-       (==)
-       (Just 3)
+  it "minimumBy three" $ do
+       minimumBy (\x -> -x) [1, 2, 3]
+       `shouldBe`
+       Just 3
 
-  test "elem' empty"
-       (elem' 1 [])
-       (==)
+  it "elem' empty" $ do
+       elem' 1 []
+       `shouldBe`
        False
 
-  test "elem' one match"
-       (elem' 1 [1])
-       (==)
+  it "elem' one match" $ do
+       elem' 1 [1]
+       `shouldBe`
        True
 
-  test "elem' one"
-       (elem' 2 [1])
-       (==)
+  it "elem' one" $ do
+       elem' 2 [1]
+       `shouldBe`
        False
 
-  test "elem' many"
-       (elem' 4 [1, 2, 3, 4, 5])
-       (==)
+  it "elem' many" $ do
+       elem' 4 [1, 2, 3, 4, 5]
+       `shouldBe`
        True
-
-test :: Show a => String -> a -> (a -> b -> Bool) -> b -> IO ()
-test d r f e =
-  if f r e
-  then putStrLn $ "PASS " ++ d
-  else do
-    putStrLn $ "FAIL " ++ d ++ ", result is: " ++ (show r)
-    exitFailure
